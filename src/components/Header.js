2a9816676bae859c1aa338db7e1c8ebbfd445c7f
irmansyaht <irmansyah.turhamun@ui.ac.id>
2019-05-09 23:39:05 +0700

    RJS 35 - Conditional Rendering

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,8 @@ class  Header extends  Component {
         this.state = {
             daftar: "Daftar Makanan Nusantara",
             datalist: this.list ,
+            statusRendering: false,
+            date: new Date()
         };
         this.handlePesan = this.handlePesan.bind(this);
     }
@@ -20,14 +22,32 @@ class  Header extends  Component {
 
     render() {
         console.log("Jalan : Render");
-        return (
+        return(
             <div>
-                <h2> Makanan Khas Indonesia </h2>
-                <p> {this.state.daftar} </p>
-                <p>{this.state.datalist}</p>
-                <a href="/" onClick={(e)=> this.handlePesan("Pesan dari Header",e)}> Halaman Header </a>
+                {
+                    this.state.statusRendering === true ? (
+                        <div>
+                        <h1>Selamat Datang</h1>
+                        <h2>Silahkan Pilih Menu </h2>
+                        </div>
+                    ):
+                    (
+                        <div>
+                    <h1> Selamat Tinggal </h1>
+                            <h2> Silahkan Datang Kembali </h2>
+                        </div>
+                    )
+                }
             </div>
         );
+        // return (
+        //     <div>
+        //         <h2> Makanan Khas Indonesia </h2>
+        //         <p> {this.state.daftar} </p>
+        //         <p>{this.state.datalist}</p>
+        //         <a href="/" onClick={(e)=> this.handlePesan("Pesan dari Header",e)}> Halaman Header </a>
+        //     </div>
+        // );
     }
 }
 export  default Header;
